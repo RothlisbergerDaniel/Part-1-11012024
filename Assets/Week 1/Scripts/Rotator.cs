1676af723b7d8eb3fe880b09e57dcaae51b5150b
RothlisbergerDaniel <dlrothlis@gmail.com>
2024-01-11 14:41:05 -0500

    Added rotator script

---
+++ Assets/Week 1/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, -speed * Time.deltaTime);
+    }
+}
