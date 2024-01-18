b91deb212f5ccf384adf3d7856b0a90eab9fefbd
RothlisbergerDaniel <dlrothlis@gmail.com>
2024-01-18 15:04:51 -0500

    Task 2 complete

---
+++ Assets/Week 1/Scripts/Switch.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+    public Color myColorOn;
+    public Color myColorOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = myColorOff;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger from " +  collision.gameObject);
+        spriteRenderer.color = myColorOn;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriteRenderer.color = myColorOff;
+    }
+}
