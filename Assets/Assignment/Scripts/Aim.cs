25aacc662322150a58a814db0f7712e7ee4b8098
DANIEL-PC\Daniel <dlrothlis@gmail.com>
2024-01-31 15:37:59 -0500

    Feature 2 complete. Turret and balls have been added.

--- Assets/Assignment/Scripts/Aim.cs
+++ Assets/Assignment/Scripts/Aim.cs
@@ -9,6 +9,8 @@ public class Aim : MonoBehaviour
     float currentRotation = 0; //current rotation
     public float speed; //rotation speed
     public float rotationCap; //rotation cap
+    public GameObject ballPrefab;
+    public Transform spawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,11 @@ public class Aim : MonoBehaviour
         currentRotation += horizontalInput * -speed * Time.deltaTime;
         currentRotation = Mathf.Clamp(currentRotation, -rotationCap, rotationCap);
         transform.rotation = Quaternion.Euler(0, 0, currentRotation); //directly sets rotation. I do it this way so I can cap rotation.
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ballPrefab, spawn.position, spawn.rotation); //spawn a ball when space key is pressed
+        }
 
     }
 }
---
+++ Assets/Assignment/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public float strength; //shot strength
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>(); //get the rigidbody of this object
+        rigidbody.AddForce(transform.up * strength, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject); //destroy self
+    }
+
+}
